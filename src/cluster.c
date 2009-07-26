c86135d52c777fcdc97e7fa7758528ff4aadfb70
Konrad Rieck <rieck@cs.tu-berlin.de>
2009-07-26 14:03:11 +0200

    forgot new files

---
+++ src/cluster.c
@@ -0,0 +1,40 @@
+/*
+ * MALHEUR - Automatic Malware Analysis on Steroids
+ * Copyright (c) 2009 Konrad Rieck (rieck@cs.tu-berlin.de)
+ * Berlin Institute of Technology (TU Berlin).
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ * --
+ */
+
+/**
+ * @defgroup cluster Clustering functionality
+ * The module contains functions for clustering feature vectors
+ * @author Konrad Rieck (rieck@cs.tu-berlin.de)
+ * @{
+ */
+
+#include "config.h"
+#include "common.h"
+#include "util.h"
+#include "cluster.h"
+
+/* External variables */
+extern int verbose;
+
+/**
+ * Clusters a set of feature vectors
+ * @param a Array of feature vectors
+ * @return clustering
+ */
+clustering_t *cluster_linkage(farray_t *fa) 
+{
+    assert(fa);
+    return NULL;
+} 
+
+/** @} */
---
+++ src/cluster.h
@@ -0,0 +1,26 @@
+/*
+ * MALHEUR - Automatic Malware Analysis on Steroids
+ * Copyright (c) 2009 Konrad Rieck (rieck@cs.tu-berlin.de)
+ * Berlin Institute of Technology (TU Berlin).
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ * --
+ */
+
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+#include "farray.h"
+
+typedef struct {
+    int *cluster;           /* Assignments of clustering */
+    unsigned long len;      /* Length of assignments */
+    unsigned long num;      /* Number of clusters */
+    double param;           /* Clustering parameter */
+} clustering_t;
+
+#endif                          /* CLUSTER_H */

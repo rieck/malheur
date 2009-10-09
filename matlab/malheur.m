e605ecc3af5a7da5cc3f651772670b804771f2a9
Konrad Rieck <rieck@cs.tu-berlin.de>
2009-10-09 15:08:53 +0200

    simplified matlab cruft

--- matlab/malheur.m
+++ matlab/malheur.m
@@ -12,8 +12,3 @@
 %     cmd:      Command to execute
 %     args:     Generic arguments
 %
-% For help see the following files:
-%   mal_load_mist.m     - Load reports in MIST format
-%   mal_distances.m     - Compute distances matrix from reports
-%   mal_dot_product.m   - Compute dot product matrix from reports
-%
